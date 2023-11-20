fc2434bd3c92d9f61dfd33ced3d9a101bd01fac5
FelipeSysten <76599815+FelipeSysten@users.noreply.github.com>
2023-11-20 07:39:31 -0300

    backend

--- src/main/java/br/com/rehab/model/Aluno.java
+++ src/main/java/br/com/rehab/model/Aluno.java
@@ -1,10 +1,16 @@
 package br.com.rehab.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 
@@ -25,6 +31,13 @@ public class Aluno {
 	private String telefone;
 	
 	private String imgUrl;
+	
+	
+	@ManyToMany
+	@JoinTable(name = "aluno_curso",
+	joinColumns = @JoinColumn(name = "aluno_id"),
+	inverseJoinColumns = @JoinColumn(name = "curso_id"))
+	private Set<Curso> cursos = new HashSet<>();
 	
 	public Aluno() {
 	
@@ -88,6 +101,13 @@ public class Aluno {
 		this.imgUrl = imgUrl;
 	}
 	
+	public Set<Curso> getCursos() {
+		return cursos;
+	}
+
+	public void setCursos(Set<Curso> cursos) {
+		this.cursos = cursos;
+	}
 	
 }
 
